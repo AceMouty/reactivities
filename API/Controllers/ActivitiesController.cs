9f1c9d886cd22ab30d21de9a8c9c636022440031
Kyle <kyleguerrero85@gmail.com>
2022-02-09 22:32:57 -0600

    feat: setup MediatR, App Extensions and Activity CRUD

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,30 +1,49 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+using Application.Activities;
+using MediatR;
 
 namespace API.Controllers
 {
     public class ActivitiesController : ReactivityBaseController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            this._context = context;
-        }
+        
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivityByID(System.Guid Id)
+        public async Task<Activity> GetActivityByID(System.Guid Id)
         {
-            return await _context.Activities.FindAsync(Id);
+            return await Mediator.Send(new Details.Query{ Id = Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewTask([FromBody] Activity activity)
+        { 
+            var result = await Mediator.Send(new Create.Command {Activity = activity});
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateActivity(Guid Id, [FromBody] Activity activity)
+        {
+            activity.Id = Id;
+            var result = await Mediator.Send(new Edit.Command { Activity = activity });
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid Id)
+        {
+            await Mediator.Send(new Delete.Command {Id = Id});
+            return Ok();
         }
     }
 }
