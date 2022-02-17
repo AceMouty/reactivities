0211999080d31caf5151b36eabd71e1efc83d6c4
Kyle <kyleguerrero85@gmail.com>
2022-02-17 00:37:34 -0600

    styling: completed activity details styling

--- client-app/src/features/activities/Details/ActivityDetailsHeader/ActivityDetailsHeader.tsx
+++ client-app/src/features/activities/Details/ActivityDetailsHeader/ActivityDetailsHeader.tsx
@@ -1,7 +1,44 @@
-import * as React from "react"
+import { observer } from 'mobx-react-lite';
+import * as React from 'react'
+import {Button, Header, Item, Segment, Image} from 'semantic-ui-react'
+import { Activity } from '../../../../app/interfaces/Activity';
+import styles from "./ActivityDetailsHeader.module.css"
 
-function ActivityDetailsHeader() {
-    return <div>Header</div>
+interface Props {
+    activity: Activity
 }
 
-export default ActivityDetailsHeader;
+// TODO: Fix image styling
+export default observer (function ActivityDetailedHeader({activity}: Props) {
+    return (
+        <Segment.Group>
+            <Segment basic attached='top' style={{padding: '0'}}>
+                <Image src={`/assets/Images/categoryImages/${activity.category}.jpg`} fluid className={styles.activityImageStyle}/>
+                <Segment className={styles.activityImageTextStyle} basic>
+                    <Item.Group>
+                        <Item>
+                            <Item.Content>
+                                <Header
+                                    size='huge'
+                                    content={activity.title}
+                                    style={{color: 'white'}}
+                                />
+                                <p>{activity.date}</p>
+                                <p>
+                                    Hosted by <strong>Bob</strong>
+                                </p>
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
+                </Segment>
+            </Segment>
+            <Segment clearing attached='bottom'>
+                <Button color='teal'>Join Activity</Button>
+                <Button>Cancel attendance</Button>
+                <Button color='orange' floated='right'>
+                    Manage Event
+                </Button>
+            </Segment>
+        </Segment.Group>
+    )
+})
