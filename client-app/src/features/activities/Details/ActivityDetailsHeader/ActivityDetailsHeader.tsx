45450b0dc6198fea1d70e0c0d0e1e43c58efa1ce
Kyle <kyleguerrero85@gmail.com>
2022-02-17 22:28:39 -0600

    styling: applied inline css overrides

--- client-app/src/features/activities/Details/ActivityDetailsHeader/ActivityDetailsHeader.tsx
+++ client-app/src/features/activities/Details/ActivityDetailsHeader/ActivityDetailsHeader.tsx
@@ -1,20 +1,31 @@
 import { observer } from 'mobx-react-lite';
-import * as React from 'react'
+import React from 'react'
 import {Button, Header, Item, Segment, Image} from 'semantic-ui-react'
-import { Activity } from '../../../../app/interfaces/Activity';
-import styles from "./ActivityDetailsHeader.module.css"
+import {Activity} from "../../../../app/interfaces/Activity";
+
+const activityImageStyle = {
+    filter: 'brightness(30%)'
+};
+
+const activityImageTextStyle = {
+    position: 'absolute',
+    bottom: '5%',
+    left: '5%',
+    width: '100%',
+    height: 'auto',
+    color: 'white'
+};
 
 interface Props {
     activity: Activity
 }
 
-// TODO: Fix image styling
 export default observer (function ActivityDetailedHeader({activity}: Props) {
     return (
         <Segment.Group>
             <Segment basic attached='top' style={{padding: '0'}}>
-                <Image src={`/assets/Images/categoryImages/${activity.category}.jpg`} fluid className={styles.activityImageStyle}/>
-                <Segment className={styles.activityImageTextStyle} basic>
+                <Image src={`/assets/Images/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle}/>
+                <Segment style={activityImageTextStyle} basic>
                     <Item.Group>
                         <Item>
                             <Item.Content>
